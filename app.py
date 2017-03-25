df82920d344a5063db509b220c7f9fc943631aca
Shashank Ojha <shashanko@gmail.com>
2017-03-25 17:32:59 -0400

    server side

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from wtforms import Form, BooleanField, TextField, PasswordField, validators
+
 
 # create the application object
 app = Flask(__name__)
+
+class LenderForm(Form):
+    amount = TextField('amount', [validators.Length(min=4, max=25)])
+    rate = TextField('rate', [validators.Length(min=6, max=35)])
+
 
 # use decorators to link the function to a url
 @app.route('/')
@@ -14,12 +21,15 @@ def index():
     return render_template('index.html')  # render a template
 
 @app.route('/lend')
-def menu():
-    return render_template('lend.html')  # render a template
+def lend():
+    return render_template('lend.html')
 
-@app.route('/borrow')
-def punjab():
-    return render_template('borrow.html')  # render a template
+@app.route('/lend', methods = ['POST'])
+def chooseLender():
+   if request.method == 'POST':
+        text = request.form['amount']
+        return render_template("lenderSearch.html", text = text)
+
 
 
 # start the server with the 'run()' method
