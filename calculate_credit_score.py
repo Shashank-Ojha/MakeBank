7499093960c44ec1bc0ff8cd25e2cbb74828aec4
Turbanator <singh.tegjeev@gmail.com>
2017-03-25 18:47:28 -0400

    Scoring

--- calculate_credit_score.py
+++ calculate_credit_score.py
@@ -18,19 +18,44 @@ def get_purchase_amounts():
     account_url="http://api.reimaginebanking.com/accounts?key=dcd6272d8dab8b826b5c1376ac90af1b"
     all_accounts=requests.get(account_url)
     all_accounts=json.loads(all_accounts.text)
-    acc=[] #list of lists. [[clothes], [groceries], [personal]]
+    users = []
     for account in all_accounts:
+        acc=[] #list of lists. [[clothes], [groceries], [personal]]
         account_id=account["_id"]
         #do something with this
         l=get_purchase_by_account(account_id)
         clothes=l[0:12]
-        groceries=l[12:-5]
-        personal=l[-5:]
+        groceries=l[12:-6]
+        personal=l[-6:]
         acc.append(clothes)
         acc.append(groceries)
         acc.append(personal)
-        print(acc) #do something with this acc
-    return l
+        users.append(acc)
+    return users
 
-(get_purchase_amounts())
+def overlayPurchases(l):
+    overlayed = []
+    for i in range(len(l[0])): #syncs to monthly basis ie clothes
+        overlayed.append(l[0][i])
+        i1 = (len(l[1])//len(l[0]))*i
+        i2 = i1 + (len(l[1])//len(l[0]))
+        overlayed[i] += sum(l[1][i1:i2])
+        if i % 2==0:
+            overlayed[i] += l[2][i//2]
+    return overlayed
+
+def getScore():
+    x = get_purchase_amounts()
+    stds = []
+    for i in x:
+        w = overlayPurchases(i) #later change to using overlay
+        mean  = sum(w)/len(w)
+        sumOfSquares = 0
+        for j in w:
+            sumOfSquares += (j-mean)**2
+        std = (sumOfSquares/len(w))**0.5
+        stds.append(std)
+    return stds
+
+print(getScore())
 
